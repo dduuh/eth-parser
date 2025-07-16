962ddfe483ec2f1337009bfbb638c789b2ab646e
David <worksilence751@gmail.com>
2025-07-16 10:36:46 +0300

    changed from primary key to unique :)

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE addresses (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL UNIQUE NOT NULL,
     address VARCHAR(255) NOT NULL,
     balance FLOAT NOT NULL,
     private_key VARCHAR(255) NOT NULL,
